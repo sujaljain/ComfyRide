ca6a2b42fce4e6be0cab45f8799c7d15ceacac72
sujaljain <sujaljain1911@gmail.com>
2025-04-08 15:28:40 +0530

    Completed 1st API for Registering the user

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,20 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const userRoutes = require('./routes/user.routes');
+
 const connectDB = require('./database/db');
 connectDB();
 
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Namaste Dunia!');
 });
+
+app.use('/users', userRoutes);
 
 module.exports = app;
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     fullName: {
@@ -20,8 +22,26 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
+        select: false,
     },
     socketId: {
         type: String,
     },
 });
+
+userSchema.methods.generateAuthToken = async function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
+    return token;
+}
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+}
+
+const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
